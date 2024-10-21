51dee55b67969aa713075bfdcd4cd74f4bf9acc5
redblackblossom <physicsdev1029@gmail.com>
2024-10-21 10:12:33 +0900

    [FEAT] 프로젝트 기본  설정 완료

---
+++ src/main/java/com/catspot/CatspotApplication.java
@@ -0,0 +1,13 @@
+package com.catspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatspotApplication.class, args);
+	}
+
+}
