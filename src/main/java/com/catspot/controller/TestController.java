0de444846429a5c006f0f9277590d783db33b6a4
redblackblossom <physicsdev1029@gmail.com>
2024-10-21 16:41:07 +0900

    [FEAT] application.properties 주입

--- src/main/java/com/catspot/controller/TestController.java
+++ src/main/java/com/catspot/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.catspot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     @GetMapping("/test")
     public String test() {
@@ -13,6 +17,6 @@ public class TestController {
 
     @GetMapping("/test2")
     public String test2() {
-        return "hello catspot!!!!!!";
+        return appName;
     }
 }
