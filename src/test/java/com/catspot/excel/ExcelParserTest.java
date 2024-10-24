cb8ba5bde10b9b6d6f275d68c7c78f9fe3322a8a
5_minsuk <oms010414@gmail.com>
2024-10-24 17:49:19 +0900

    [TEST] 파서 테스트코드 작성

--- src/test/java/com/catspot/excel/ExcelParserTest.java
+++ src/test/java/com/catspot/excel/ExcelParserTest.java
@@ -1,30 +1,72 @@
 package com.catspot.excel;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ExcelParserTest {
 
     @Test
-    public void parserTest(){
+    public void 포맷이_두개지요(){
         ExcelParser ep = new ExcelParser();
-        List<Schedule> ret = ep.parse("수1(N411), 금1~2(N411)");
-        for (Schedule r : ret) {
-            System.out.println("r = " + r);
-        }
+        Schedule[] ret = ep.parse("수1(N411), 금1~2(N311)").toArray(new Schedule[0]);
+
+        assertEquals(ret.length, 3);
+        check(ret[0], "수", 1, "N", "N411","411");
+        check(ret[1], "금", 1, "N", "N311", "311");
+        check(ret[2], "금", 2, "N", "N311", "311");
+
     }
 
     @Test
-    public void parserTest1(){
+    public void 단일_포맷_일반(){
         ExcelParser ep = new ExcelParser();
-        List<Schedule> ret = ep.parse("수1(), 금1~2(N411)");
-        for (Schedule r : ret) {
-            System.out.println("r = " + r);
+        Schedule[] ret = ep.parse("수1(N123)").toArray(new Schedule[0]);
+        assertEquals(ret.length, 1);
+        check(ret[0], "수", 1, "N", "N123","123");
+    }
+    
+    @Test
+    public void 단일_포맷_강의실없음(){
+        ExcelParser ep = new ExcelParser();
+        Schedule[] ret = ep.parse("수1()").toArray(new Schedule[0]);
+        assertEquals(ret.length, 1);
+        check(ret[0], "수", 1, null, null,null);
+    }
+
+    @Test
+    public void 테니스장은_몰랐지(){
+        //테니스장, 헬스장, 체육관
+        ExcelParser ep = new ExcelParser();
+        Schedule[] ret = ep.parse("월3(테니스장)").toArray(new Schedule[0]);
+        check(ret[0], "월", 3, null, "테니스장",null);
+    }
+
+
+    public void check(Schedule s, String day, Integer time, String buildingName, String classroomName, String classroomNumber){
+        assertEquals(Integer.parseInt(s.getTime()),time);
+        assertEquals(s.getDay(),day);
+
+        String bname = s.getLocation().getBuildingName();
+        assertEquals(bname,buildingName);
+
+        String cname = s.getLocation().getClassroomName();
+        assertEquals(cname,classroomName);
+
+        Integer cnum = s.getLocation().getClassroomNumber();
+        if(cnum==null){
+            assertEquals(cnum, classroomNumber);
         }
+        else{
+            assertEquals(Integer.toString(cnum),classroomNumber);
+        }
+
     }
 }
