8444a725591733851786af5e7f8053e9ef511191
SeoYoung <seoyoung7623@naver.com>
2024-10-24 12:44:01 +0900

    [FEAT] 파싱데이터 저장

--- src/main/java/com/catspot/excel/ExcelReader.java
+++ src/main/java/com/catspot/excel/ExcelReader.java
@@ -1,39 +1,87 @@
 package com.catspot.excel;
 
+import lombok.AllArgsConstructor;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class ExcelReader {
+    private static final int NAMECELLNUMBER = 3;
+    private static final int SCHEDULECELLNUMBER = 9;
+
+    private final ExcelParser excelParser;
+    private final ClassroomRepository classroomRepository;
+    private final TimeTableRepository timeTableRepository;
+    private final SessionTimeRepository sessionTimeRepository;
     public void importClassrooms(InputStream inputStream) {
         try {
             Workbook workbook = WorkbookFactory.create(inputStream);
             Sheet sheet = workbook.getSheetAt(0);
-            // test
-            for (int rowIndex = 2; rowIndex <= 10; rowIndex++) {
+            for (int rowIndex = 2; rowIndex < sheet.getLastRowNum(); rowIndex++) {
                 Row row = sheet.getRow(rowIndex);
-
-                // 행이 null일 수 있으므로 체크
                 if (row == null) continue;
-
                 // 교과목명
-                Cell nameCell = row.getCell(3);
+                Cell nameCell = row.getCell(NAMECELLNUMBER);
 
                 // 요일및교시(강의실)
-                Cell scheduleCell = row.getCell(9);
+                Cell scheduleCell = row.getCell(SCHEDULECELLNUMBER);
 
-                // 셀이 null일 경우도 대비
-                String schedule = (scheduleCell != null) ? scheduleCell.getStringCellValue() : "";
-                String name = (nameCell != null) ? nameCell.getStringCellValue() : "";
+                if (scheduleCell == null || scheduleCell.getStringCellValue().trim().isEmpty() ||
+                        nameCell == null || nameCell.getStringCellValue().trim().isEmpty()) {
+                    continue;
+                }
+                String schedule = scheduleCell.getStringCellValue();
+                String name = nameCell.getStringCellValue();
 
-                System.out.println(name + " " + schedule);
+                List<Schedule> scheduleList = excelParser.parse(schedule);
+
+                for (Schedule s : scheduleList) {
+                    Optional<Classroom> existingClassroom = getClassroom(s);
+
+                    Classroom classroom;
+                    if (existingClassroom.isPresent()) {
+                        classroom = existingClassroom.get();
+                    } else {
+                        classroom = classroomRepository.save(s.getLocation());
+                    }
+
+                    SessionTime sessionTime = sessionTimeBuilder(s);
+                    sessionTimeRepository.save(sessionTime);
+
+                    TimeTable timeTable = timeTableBuilder(s, name ,classroom, sessionTime);
+                    timeTableRepository.save(timeTable);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    // 엔티티 매핑, 강의실 저장
+    private Optional<Classroom> getClassroom(Schedule s) {
+        Optional<Classroom> existingClassroom = classroomRepository.findByClassroomName(s.getLocation().getClassroomName());
+        return existingClassroom;
+    }
+
+    private SessionTime sessionTimeBuilder(Schedule s) {
+        SessionTime sessionTime = SessionTime.builder()
+                .time(s.getTime())
+                .build();
+        return sessionTime;
+    }
+
+    private TimeTable timeTableBuilder(Schedule s, String name ,Classroom classroom, SessionTime sessionTime) {
+        TimeTable timeTable = TimeTable.builder()
+                .day(s.getDay())
+                .classroom_name(name)
+                .classroom(classroom)
+                .sessionTime(sessionTime)
+                .build();
+        return timeTable;
+    }
+
 }
