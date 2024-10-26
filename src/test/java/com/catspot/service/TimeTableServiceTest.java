1570462ff8548b9c2b21751d4986093487f20441
5ms <86279729+oms01@users.noreply.github.com>
2024-10-27 03:02:18 +0900

    [TEST] TimeTableService Test Coverage 100 (#34)

--- src/test/java/com/catspot/service/TimeTableServiceTest.java
+++ src/test/java/com/catspot/service/TimeTableServiceTest.java
@@ -1,6 +1,7 @@
 package com.catspot.service;
 
 import com.catspot.dto.response.TimeTableGetResponse;
+import com.catspot.exceptionhandler.CustomException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,5 +23,27 @@ class TimeTableServiceTest {
 
         assertEquals("N113", response.getClassName());
         System.out.println("response = " + response.getClassName());
+    }
+
+    @Test
+    void testGetTimeTableList_InvalidBuildingName() {
+        String buildingName = "";
+        int classroomNumber = 303;
+        String day = "Monday";
+
+        assertThrows(CustomException.class, () -> {
+            timeTableService.getTimeTableList(buildingName, classroomNumber, day);
+        });
+    }
+
+    @Test
+    void testGetTimeTableList_InvalidClassroomNumber() {
+        String buildingName = "A";
+        int classroomNumber = -1; // 유효하지 않은 강의실 번호
+        String day = "Monday";
+
+        assertThrows(CustomException.class, () -> {
+            timeTableService.getTimeTableList(buildingName, classroomNumber, day);
+        });
     }
 }
