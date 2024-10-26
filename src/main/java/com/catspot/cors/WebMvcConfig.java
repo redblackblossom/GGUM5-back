76e211a49f6c30eefca3bedfad2a0612b39f4f5c
Solf <74342496+SolfE@users.noreply.github.com>
2024-10-26 19:16:09 +0900

    [FIX] CORS 설정 변경

---
+++ src/main/java/com/catspot/cors/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.catspot.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("GET")
+                .allowedOrigins("http://localhost:5173", "https://catspot-dev.web.app");
+    }
+}
