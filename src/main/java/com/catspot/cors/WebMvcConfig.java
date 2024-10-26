c8d2414b1d7a8a3d490b1c1063402f6514d9c0a1
Solf <74342496+SolfE@users.noreply.github.com>
2024-10-26 19:55:56 +0900

    [FIX] CORS POST 허용

--- src/main/java/com/catspot/cors/WebMvcConfig.java
+++ src/main/java/com/catspot/cors/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedMethods("GET")
+                .allowedMethods("GET", "POST")
                 .allowedOrigins("http://localhost:5173", "https://catspot-dev.web.app");
     }
 }
