478631b74d86458bb771f0100457068342ec3c33
SeoYoung <seoyoung7623@naver.com>
2024-10-26 14:05:05 +0900

    [REFACTOR] 시간을 교시로 변경 메서드

--- src/main/java/com/catspot/util/TimeCalculator.java
+++ src/main/java/com/catspot/util/TimeCalculator.java
@@ -1,37 +1,10 @@
 package com.catspot.util;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.TextStyle;
-import java.util.Locale;
-
 public class TimeCalculator {
-    private static String testDay = null;
-    private static Integer testTime = null;
     private static final int START_HOUR = 9;
 
-    public static int getCurrentTime() {
-        LocalTime now = LocalTime.now();
-        int hour = now.getHour();
-
-        return (testTime != null) ? testTime : calculatePeriod(hour);
-    }
-
-    public static String getCurrentDay(){
-        DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
-        String day = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN);
-        return (testDay != null) ? testDay: day;
-    }
-
-    public static void resetTestConditions() {
-        testDay = null;
-        testTime = null;
-    }
-
-    public static void setTestConditions(String day, Integer time) {
-        testDay = day;
-        testTime = time;
+    public static int getCurrentTime(int hour) {
+        return calculatePeriod(hour);
     }
 
     private static int calculatePeriod(int hour) {
